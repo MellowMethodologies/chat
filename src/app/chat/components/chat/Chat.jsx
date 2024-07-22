d34dc8ccdacdbf8852c7f6132e07c7dd9b138319
Saad Badr <sbadr@e3r8p7.1337.ma>
2024-07-22 09:48:17 +0100

    hcat

--- src/app/chat/components/chat/Chat.jsx
+++ src/app/chat/components/chat/Chat.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import style from './Chat.module.css'
+import style from './Chat.module.scss'
 
 const Chat = () => {
   return (
--- src/app/chat/components/detail/Detail.jsx
+++ src/app/chat/components/detail/Detail.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import style from './Detail.module.css'
+import style from './Detail.module.scss'
 
 
 export default function detail() {
